636b9dd1163fcef838515e280a28bc5f5073acb6
RitikaJoshi28 <81311833+RitikaJoshi28@users.noreply.github.com>
2021-05-08 01:47:41 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,39 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.image=loadImage("block.png");
+      this.visibility=255;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+   
+    
+    display()
+    {
+      console.log(this.body.speed);
+      if(this.body<3);
+      {
+        super.display();
+      
+      }
+      else{
+        World.remove(world,this.body);
+        push();
+        this.Visibility=-5;
+        tint(255,this.Visibility);
+        image(this.image,this.body.position.x,this.body.position.y,50,50);
+        pop();
+      }
+    
+       
+
+      var pos= this.body.position;
+      rectMode(CENTER);
+        rect(pos.x,pos.y,this.width, this.height)
+    }
+}
